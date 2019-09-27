6fb332735790bbda7bef6c8966dbdf29fbe8f7ea
edizcelik <mr.edizcelik@gmail.com>
2019-09-27 11:17:58 +0300

    Yigit hand gesture v1

--- js/record.js
+++ js/record.js
@@ -7,6 +7,9 @@ const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight - HEADER_HEIGHT;
 const canvasHiddenClass = "canvas-hidden";
 const THRESHOLD = 4.5;
+const MAX_HAND_BOTTOM = 107;
+const handImg = document.querySelector(".finger");
+let shouldStopHandAnimation = false;
 
 function preload() {
   // song = loadSound(‘assets/lucky_dragons_-_power_melody.mp3’);
@@ -26,20 +29,42 @@ function draw() {
 
   data.push(noiseLevel);
 
-  fill(127);
-  stroke(0);
+  // fill(127);
+  // stroke(0);
   // Draw an ellipse with size based on volume
-  ellipse(canvasWidth / 2, canvasHeight / 2, canvasWidth - 20, noiseLevel * 2);
+  // ellipse(canvasWidth / 2, canvasHeight / 2, canvasWidth - 20, noiseLevel * 2);
+
+  console.log(noiseLevel)
+  if (!shouldStopHandAnimation && handImg) {
+    if (noiseLevel < 4) {
+      handImg.style.bottom = -350;
+    } else {
+      handImg.style.bottom = generateHandBottomValueFromNoise(noiseLevel);
+    }
+  }
+}
+
+function generateHandBottomValueFromNoise(noise) {
+  let bottom = noise /20;
+
+  if (MAX_HAND_BOTTOM > bottom) {
+    bottom = MAX_HAND_BOTTOM;
+  }
+
+  return bottom;
 }
 
 setInterval(() => {
   const avr = data.reduce((sum, item) => sum + item, 0) / data.length || 0;
 
-  console.log(avr);
+  // console.log(avr);
   if (avr > THRESHOLD) {
-    document.body.classList.add(canvasHiddenClass);
+    handImg.style.bottom = MAX_HAND_BOTTOM;
+    shouldStopHandAnimation = true;
+    // document.body.classList.add(canvasHiddenClass);
   } else {
-    document.body.classList.remove(canvasHiddenClass);
+    shouldStopHandAnimation = false;
+    // document.body.classList.remove(canvasHiddenClass);
   }
 
   data = [];
